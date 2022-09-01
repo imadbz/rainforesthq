02db99429379381d48d884d2d43e40caa9ef0463
Imad Bouziani <imad.a.bouziani@gmail.com>
2022-09-02 00:12:00 +0800

    convert to typescript

--- api/index.js
+++ api/index.js
@@ -3,9 +3,9 @@ var data = require('./data.json');
 var app = express();
 
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', "*");
-  res.header('Access-Control-Allow-Methods','GET');
+  res.header('Access-Control-Allow-Methods', 'GET');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import React from "react";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code> src / App.js </code> and save to reload.
         </p>
       </header>
     </div>
